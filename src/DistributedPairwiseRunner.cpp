7a8770522543c3f86d9b34e7dd5924e31624b8c8
gulsumgudukbay <gudukbay@gmail.com>
2021-04-28 04:42:37 -0400

    obtained dfd_col and dfd_row for gpu transfer

--- src/DistributedPairwiseRunner.cpp
+++ src/DistributedPairwiseRunner.cpp
@@ -9,6 +9,7 @@
 #include "../kernels/allocate_stringset.cuh"
 #include <atomic> // std::atomic, std::atomic_flag, ATOMIC_FLAG_INIT
 #include <iostream>
+#include <string)
 
 DistributedPairwiseRunner::DistributedPairwiseRunner(
 	const std::shared_ptr<DistributedFastaData> dfd,
@@ -373,13 +374,13 @@ void DistributedPairwiseRunner::run_batch(
 		
 		int len_col_seq = (dfd->col_seqs).length();
 		int len_row_seq = (dfd->row_seqs).length();
-		char ** dfd_col_seq_gpu = (char**) malloc(sizeof(char*) * len_col_seq; //CHECK and OBTAIN
+		char ** dfd_col_seq_gpu = (char**) malloc(sizeof(char*) * len_col_seq); //CHECK and OBTAIN
 		char ** dfd_row_seq_gpu = (char**) malloc(sizeof(char*) * len_row_seq);
 
 		for(int dfd_i = 0; dfd_i < len_col_seq; dfd_i++)
-			dfd_col_seq_gpu[dfd_i] = (dfd->col_seqs)[dfd_i];
+			dfd_col_seq_gpu[dfd_i] = string((dfd->col_seqs)[dfd_i]);
 		for(int dfd_i = 0; dfd_i < len_row_seq; dfd_i++)
-			dfd_row_seq_gpu[dfd_i] = (dfd->row_seqs)[dfd_i];	
+			dfd_row_seq_gpu[dfd_i] = string((dfd->row_seqs)[dfd_i]);	
 
 		//cuda call for fillStringset goes here
 		//fillStringSetCuda(beg, end, local_nnz_count, seqsh_gpu, seqsv_gpu, lids, mattuples0, mattuples1, cks_count, row_offset, col_offset, ckthr, dfd_col_seq_gpu, dfd_row_seq_gpu, algn_cnts);
