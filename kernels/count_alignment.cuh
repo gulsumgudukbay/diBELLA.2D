c9a389a8ad563b53edec5dd0b483ce2638463aee
Yilin Feng <fengyilin118@gmail.com>
2021-05-03 18:32:41 -0400

    count alignment changes

--- kernels/count_alignment.cuh
+++ kernels/count_alignment.cuh
@@ -5,7 +5,7 @@
 #include "device_launch_parameters.h"
 
 void count_alignment_cuda(int batch_size, int local_nnz_count, int* mattuples0, int* mattuples1, int* cks_count,
-    int col_offset,int row_offset, int ckthr, int* align_batch, int* elimi_batch);
+    int col_offset,int row_offset, int ckthr, int* align_batch);
 
 void test();
 
